357cc7818464761656b4e462d76396e1eccfd84f
Thien Nguyen <19522262@gm.uit.edu.vn>
2021-04-29 19:47:33 +0700

    Ver1

---
+++ UI/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime ass = dateTimePicker1.Value;
+
+            string sql = "INSERT INTO `MEMBER` (MEMID, NAME, DOB, NUMBER_OF_BOOK_ALLOWED, EMAIL, ADDRESS, MEM_IMG_ID) " +
+              $"VALUES ('{textBox1.Text}', '{textBox2.Text}', '{dateTimePicker1.Value}', '{textBox3.Text}', '{textBox4.Text}', '{textBox5.Text}', '{textBox6.Text}');";
+            MySqlCommand cmd = new MySqlCommand(sql, Program.connection);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            rdr.Close();
+            MessageBox.Show("Added");
+        }
+    }
+}
---
+++ UI/WindowsFormsApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace WindowsFormsApp2
+{
+    static class Program
+    {
+        public static string server = "sql6.freesqldatabase.com";
+        public static string database = "sql6405406";
+        public static string uid = "sql6405406";
+        public static string password = "N6lh1TIywW";
+        public static string connectionString;
+        public static MySql.Data.MySqlClient.MySqlConnection connection;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+       
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
