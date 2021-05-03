9a9597a22a6548e95a6092761ee09a8750e45f0e
asdqwe02 <shaunakathien@gmail.com>
2021-05-03 21:36:43 +0700

    added Member and Book Class

---
+++ UI/LibraryManagementClasses/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementClasses
+{
+    public class Book
+    {
+		string NAME;
+		string PUBLISHER;
+		string AUTHOR;
+		bool AVAILABLE;
+		string CATEGORY;
+		double PRICE;
+		int year;
+		string BOOK_IMG_ID;
+        public Book()
+        {
+            NAME = "unknown";
+            PUBLISHER = "unknown";
+            AUTHOR = "unknow";
+            AVAILABLE = false;
+            CATEGORY = "unknown";
+            PRICE = 0;
+            year = 0;
+            BOOK_IMG_ID = "PATH";
+        }
+
+        public Book(string Name, string Pub, string Author, bool Status, string Category, double price, int year,string image_id)
+        {
+            NAME = Name;
+            PUBLISHER = Pub;
+            AUTHOR = Author;
+            AVAILABLE = Status;
+            CATEGORY = Category;
+            PRICE = price;
+            year = year;
+            // path is AppDomain.CurrentDomain.BaseDirectory + "/Book_Images/" + BOOK_IMG_ID;
+            BOOK_IMG_ID = image_id;
+        }
+        public bool Eligible_To_Borrow(Member obj, string Table)
+        {
+            return true;
+        }
+
+    }
+}
---
+++ UI/LibraryManagementClasses/Member.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementClasses
+{
+	public class Member
+	{
+		string NAME;
+		DateTime DOB;
+		int NUMBER_OF_BOOK_ALLOWED;
+		string EMAIL;
+		string ADDRESS;
+		string MEM_IMG_ID;
+		public Member()
+		{
+			NAME = "unknown";
+			DOB = DateTime.Today;
+			NUMBER_OF_BOOK_ALLOWED = 0;
+			EMAIL = "unknown";
+			ADDRESS = "unknown";
+			MEM_IMG_ID = "PATH";
+		}
+
+		public Member(string name,DateTime DoB, int Number_Of_Book_Allowed, string Email, string Address)
+		{
+			NAME = name;
+			DOB = DoB;
+			NUMBER_OF_BOOK_ALLOWED = Number_Of_Book_Allowed;
+			EMAIL = Email;
+			ADDRESS = Address;
+			// path is AppDomain.CurrentDomain.BaseDirectory + "/Member_Images/" + EMAIL
+			int index = Email.IndexOf("@");
+			if (index > 0)
+			MEM_IMG_ID = Email.Substring(0, index);
+		}
+	}
+
+}
